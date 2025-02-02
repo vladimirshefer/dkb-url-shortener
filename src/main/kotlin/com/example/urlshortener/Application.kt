51ecd495a62225036484485371c83ddfcaff84a5
Vladimir Shefer <vladimir.shefer@jetbrains.com>
2025-02-02 14:00:12 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/urlshortener/Application.kt
@@ -0,0 +1,11 @@
+package com.example.urlshortener
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main() {
+    SpringApplication.run(Application::class.java)
+}
