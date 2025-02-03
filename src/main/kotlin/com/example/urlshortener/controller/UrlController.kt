d84861deaa6dcee82962b17855bb581d618b57d8
Vladimir Shefer <vladimir.shefer@jetbrains.com>
2025-02-03 14:23:10 +0100

    Add Cache-Control for url get

--- src/main/kotlin/com/example/urlshortener/controller/UrlController.kt
+++ src/main/kotlin/com/example/urlshortener/controller/UrlController.kt
@@ -3,10 +3,12 @@ package com.example.urlshortener.controller
 import com.example.urlshortener.service.UrlService
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.servlet.http.HttpServletResponse.SC_BAD_REQUEST
+import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import java.net.URI
+import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/url")
@@ -33,6 +35,7 @@ class UrlController(
         val fullUrl = urlService.get(id) ?: throw ResponseStatusException(HttpServletResponse.SC_NOT_FOUND, "Invalid id: $id", null)
 
         return ResponseEntity.status(302)
+            .cacheControl(CacheControl.maxAge(30, TimeUnit.DAYS))
             .location(URI.create(fullUrl))
             .build()
     }
